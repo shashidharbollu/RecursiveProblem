65d245bc6633eca2253157d6694bd4ec413f11ec
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-12 23:48:29 +0530

    [shashidhar] Add : factorial programme without recursive function is updated

---
+++ FactorialProblem/NormalFactorial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialProblem
+{
+    public class NormalFactorial
+    {
+        public void Factoriall()
+        {
+            double num, factorial = 1;
+            Console.WriteLine("enter your number : ");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("cant find the factorial for a negative number");
+            }
+            else if (num <= 1)
+            {
+                Console.WriteLine("0! = 1");
+            }
+            else
+            {
+                for (double counter = num; counter >= 2; counter--)
+                {
+                    factorial = factorial * counter;
+                }
+                Console.WriteLine(factorial);
+            }
+
+        }
+    }
+}
+
---
+++ FactorialProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FactorialProblem;
+
+
+class Program
+{
+    static void Main(String[] args)
+    { 
+    NormalFactorial factorial = new NormalFactorial();
+        factorial.Factoriall();
+
+    }
+}
+        
